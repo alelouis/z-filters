3cfa1ed8e19aa5a1080f7c8796334283550d2659
alelouis <me@alelouis.eu>
2022-05-29 21:37:10 +0200

    More natural colors for zeros and poles

--- plots.py
+++ plots.py
@@ -10,6 +10,7 @@ prop = font_manager.FontProperties(fname=font_path)
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = prop.get_name()
 
+
 def show_surface(extent, zeros, poles):
     lbo, ubo = extent
     x, y = np.meshgrid(np.linspace(lbo, ubo, 1000), np.linspace(lbo, ubo, 1000))
@@ -18,7 +19,7 @@ def show_surface(extent, zeros, poles):
     t = np.linspace(0, 2 * np.pi, 100)
     plt.figure(dpi=100, figsize=(6, 3))
     plt.subplot(1, 2, 1)
-    plt.imshow(np.log(np.abs(h_z)), cmap='Greys', extent=[lbo, ubo, lbo, ubo])
+    plt.imshow(np.log(np.abs(h_z)), cmap='Greys_r', extent=[lbo, ubo, lbo, ubo])
     plt.plot(np.cos(t), np.sin(t), c='w', alpha=0.8, linewidth=0.5)
     plt.scatter(np.real(zeros), np.imag(zeros), c='r', s=0.5)
     plt.scatter(np.real(zeros), -np.imag(zeros), c='r', s=0.5)
