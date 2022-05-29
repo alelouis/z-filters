68b402ae8dd9d06e86aebaf9c868b24f09b7b2c5
alelouis <me@alelouis.eu>
2022-05-29 21:41:45 +0200

    Adds documentation for filters.py

--- filters.py
+++ filters.py
@@ -5,6 +5,13 @@ import sympy
 
 
 def compute_h_z(a, b, z):
+    """
+    Computes complex transfer function on z
+    :param a: denominator coefficients
+    :param b: numerator coefficients
+    :param z: np.array of input z
+    :return: num/den transfer function
+    """
     num = np.zeros_like(z, dtype=np.complex128)
     den = np.zeros_like(z, dtype=np.complex128)
 
@@ -18,6 +25,12 @@ def compute_h_z(a, b, z):
 
 
 def compute_b_a(zeros, poles):
+    """
+    Compute B/A polynomial coefficients from zeros and poles
+    :param zeros: complex zeros
+    :param poles: complex poles
+    :return: b, a coefficients
+    """
     q = Poly(1, x)
     for zero in zeros:
         q = q.mul(Poly(x - zero, x)).mul(Poly(x - np.conjugate(zero), x))
@@ -40,6 +53,13 @@ def compute_b_a(zeros, poles):
 
 
 def filter_b_a(c, a, b):
+    """
+    Compute FIR and IIR outputs
+    :param c: input signal
+    :param a: numerator coefficients
+    :param b: denominator coefficients
+    :return: response signal
+    """
     n = c.size
     warm_up = max(a.size, b.size)
     response = np.zeros(warm_up + n)
